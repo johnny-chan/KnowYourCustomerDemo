909a29ce6f2383fdcb6a4055c20e7e079ef056ff
Johnny Chan <johnny.chan@redington.co.uk>
2018-02-23 22:49:31 +0000

    updated start config to use custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
 import * as React from 'react';
+// import { Base64 } from 'js-base64';
+
 import './App.css';
 
 const logo = require('./logo.svg');
 
-class App extends React.Component {
+// const apiKey: string = 'lFy9GaaZ9jC08EMPA7JOnjmhy3qy6VQmlzatBmOn';
+
+interface Props {
+  searchTerm: string; 
+}
+
+interface State {
+
+  // tslint:disable-next-line:no-any
+  companies: any[]; 
+}
+
+const requestOptions = {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    // authorization: `BASIC ${Base64.encode(apiKey)}`
+    authorization: 'Basic bEZ5OUdhYVo5akMwOEVNUEE3Sk9uam1oeTNxeTZWUW1semF0Qm1Pbjo=',
+  }  
+}; 
+
+class App extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`https://api.companieshouse.gov.uk/search?q=${this.props.searchTerm}`, requestOptions);
+
+    const result = await response.json();
+
+    this.setState( { companies: result.items});
+
+    // tslint:disable-next-line:no-console
+    console.log('companies:' + JSON.stringify(this.state.companies));
+  }
+
   render() {
     return (
       <div className="App">
