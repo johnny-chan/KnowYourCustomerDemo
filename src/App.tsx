ab6b02e2dbdd8a7310b66a541c51deaa50735469
JC <jcwc1hotmail.com>
2018-03-03 15:48:16 +0000

    fixed fetch companies house api query

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,18 @@ interface Props {
   searchTerm: string;
 }
 
+interface CompanyHouseItemApiResponse {
+  title: string;
+  company_number: string;
+}
+
 interface CompanyDetail {
   name: string;
   companyNumber: string;
 }
 
 interface State {
-  // tslint:disable-next-line:no-any
-  companies: CompanyDetail[];
+  companies: Array<CompanyDetail>;
 }
 
 const requestOptions = {
@@ -26,50 +30,33 @@ const requestOptions = {
   headers: {
     Accept: 'application/json',
     // authorization: `BASIC ${Base64.encode(apiKey)}`
-    authorization: 'basic bEZ5OUdhYVo5akMwOEVNUEE3Sk9uam1oeTNxeTZWUW1semF0Qm1Pbjo='
+    authorization: 'Basic bEZ5OUdhYVo5akMwOEVNUEE3Sk9uam1oeTNxeTZWUW1semF0Qm1Pbjo='
   }
 };
+
+async function getCompanyHouseDetails(searchTerm: string) {
+  const res = await fetch(
+    `https://api.companieshouse.gov.uk/search?q=${searchTerm}`,
+    requestOptions
+  );
+  const json = await res.json();
+  return json;
+}
 
 export default class App extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-  }
 
-  async componentDidMount() {
-    const response = await fetch(
-      `https://api.companieshouse.gov.uk/search?q=${this.props.searchTerm}`,
-      requestOptions
-    );
-
-    const result = await response.json();
-
-    const companyDetails: Array<CompanyDetail> = result.items.map(
-      (item: { title: string; company_number: string }) => {
+    getCompanyHouseDetails(this.props.searchTerm).then(response => {
+      const results = response.items.map((item: CompanyHouseItemApiResponse) => {
         return {
           name: item.title,
           companyNumber: item.company_number
         };
-      }
-    );
+      });
 
-    // tslint:disable-next-line:no-console
-    console.log(`name is ${companyDetails[0].name}`);
-    // tslint:disable-next-line:no-console
-    console.log(`name is ${companyDetails[0].companyNumber}`);
-    // how can you iterate through items and convert to companyDetail type
-    // const companyDetails: Array<CompanyDetail> =  JSON.parse(JSON.stringify(result.items));
-    //   const companies: CompanyDetail[] = items.map(item => <CompanyDetail>{
-    //   name : item.title,
-    //   number: item.company_number
-    // });
-
-    // tslint:disable-next-line:no-console
-    // console.log(`Items is ${JSON.stringify(companyDetails)}`);
-
-    this.setState({ companies: companyDetails });
-
-    // tslint:disable-next-line:no-console
-    // console.log('companies:' + JSON.stringify(this.state.companies));
+      this.setState({ companies: results });
+    });
   }
 
   render() {
