8803a776571aa42328e86fa99ca78c94d7b49af7
Johnny Chan <johnny.chan@redington.co.uk>
2018-03-27 13:03:21 +0100

    renamed CompanyDetailsReducer to SearchResultsReducer

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
-import { reducer as CompanyDetailsReducer, CompanyDetailsState } from './CompanyDetailsReducer';
+import { reducer as SearchResultsReducer, CompanyDetailsState } from './SearchResultsReducer';
 
 export interface RootState {
   companyDetailsList: CompanyDetailsState;
 }
 
 export const rootReducer = combineReducers<RootState>({
-  companyDetailsList: CompanyDetailsReducer
+  companyDetailsList: SearchResultsReducer
 });
