06f9e43260ce3571018f6cbdaaef6ded8b63d303
Johnny Chan <jcwc1@hotmail.com>
2018-05-07 22:36:57 +0100

    removed footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import * as React from 'react';
-// import Paper from 'material-ui/Paper';
-// import { Card, CardText } from 'material-ui/Card';
-// import ToolBarGroup from 'material-ui/Toolbar/ToolbarGroup';
-// import ToolBar from 'material-ui/Toolbar';
+
 import Header from './components/header';
 import Search from './containers/Search';
-import Footer from './components/footer';
-
-// import SearchResult from './containers/SearchResult';
-// import CompanyDetail from './containers/CompanyDetail';
-
-// import './App.css';
+// import Footer from './components/footer';
 
 interface Props {}
 
@@ -24,7 +16,7 @@ export default class App extends React.Component<Props> {
       <>
         <Header />
         <Search />
-        <Footer />
+        {/* <Footer /> */}
       </>
     );
   }
